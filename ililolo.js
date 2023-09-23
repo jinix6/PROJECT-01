e1aae10d6d1874be23194fdb39fea8df82493f27
jinix6 <135565126+jinix6@users.noreply.github.com>
2023-09-23 08:04:25 +0530

    Create ililolo.js

---
+++ ililolo.js
@@ -0,0 +1,45 @@
+function sjjnz(){
+  
+  alert("jsjsj")
+}
+function clin1(){
+  
+  alert("jsjsj")
+}
+
+function cancelsjsj() {
+  document.getElementById("nsjn").style.animation ="dialog1hide 250ms 1 forwards";
+}
+function showsjsjsj() {
+  document.getElementById("nsjn").style.animation ="dialog1show 250ms 1 forwards";
+}
+function sjsjsnshndehb() {
+window.open("freefire://process?region=IND&lang=en&action=ugc_mapdetail&map_code=%23FREEFIREB51B81A24D5E68255CA71935D89F65757663&version=OB40");
+cancelsjsj();
+}
+function ndjdnjsj() {
+  document.getElementById("LKDKKSKK").style.animation ="dialog1hide 250ms 1 forwards";
+}
+
+function nfizjshhhd() {
+  document.getElementById("LKDKKSKK").style.animation ="dialog1show 250ms 1 forwards";
+}
+function ejudhd(){
+  window.open("https://t.me/freefirecraftlandgroup");
+}
+function djdjndh(){
+  window.open("https://www.instagram.com/jinxx6_6?r=nametag");
+}
+function sjsjsjjdj(){
+  document.getElementById("nxidndnd").style.animation ="dialog1hide 250ms 1 forwards";
+}
+function hufgt(){
+  document.getElementById("kdkdjzj").style.animation ="dialog1show 250ms 1 forwards";
+}
+function neidndnsj(){
+document.getElementById("kdkdjzj").style.animation ="dialog1hide 250ms 1 forwards";}
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
+    
